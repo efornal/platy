7999a0106a61c053396c23001227c9e5ab66d07b
esteban <efornal@gmail.com>
2016-08-03 08:54:15 -0300

    Static configuration files in settings

--- platy/settings.py
+++ platy/settings.py
@@ -134,6 +134,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+#    '/static/',
+#)
+
 LOCALE_PATHS = (
      BASE_DIR + '/locale', )
 

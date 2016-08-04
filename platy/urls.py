9e0a24a66a6c2bd6819da22800fcf30f5f1a1e67
esteban <efornal@gmail.com>
2016-08-04 11:43:58 -0300

    redirection root to admin

--- platy/urls.py
+++ platy/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(pattern_name='admin:index', permanent=False))
 ]
+

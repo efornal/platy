9d7e082e18cdf2f87789e8ea37db5c036aed4e3d
esteban <efornal@gmail.com>
2017-06-14 12:37:17 -0300

    added ForceLangMiddleware

--- platy/settings.tpl.py
+++ platy/settings.tpl.py
@@ -53,6 +53,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app.middleware.ForceLangMiddleware',
 ]
 
 ROOT_URLCONF = 'platy.urls'
